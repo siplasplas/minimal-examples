1bbeb556b4e204de7e43657afd4aa11d7f584577
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-08-03 13:47:42 +0200

    solution : Q_INIT_RESOURCE

--- Qt/static-lib-res6/lib/LibWindow.cpp
+++ Qt/static-lib-res6/lib/LibWindow.cpp
@@ -1,11 +1,17 @@
 #include "LibWindow.h"
 #include <QPushButton>
 
+void initLibraryResources() {
+    Q_INIT_RESOURCE(2);
+}
+
+Q_CONSTRUCTOR_FUNCTION(initLibraryResources)
 LibWindow::LibWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     QPushButton *button = new QPushButton(this);
     QIcon icon;
     icon.addFile(":2.png", QSize(48, 48));
+    button->setIcon(icon);
     setCentralWidget(button);
 }
