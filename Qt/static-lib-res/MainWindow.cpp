9862292bfd00d37b6f050a434d29d4b69c3142af
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-08-03 10:51:05 +0200

    try add icons to both  app and library

--- Qt/static-lib-res/MainWindow.cpp
+++ Qt/static-lib-res/MainWindow.cpp
@@ -5,6 +5,9 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     QPushButton *button = new QPushButton(this);
+    QIcon icon;
+    icon.addFile(":1.png", QSize(48, 48));
     setCentralWidget(button);
+    button->setIcon(icon);
     lw.show();
 }
--- Qt/static-lib-res/lib/LibWindow.cpp
+++ Qt/static-lib-res/lib/LibWindow.cpp
@@ -5,5 +5,7 @@ LibWindow::LibWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     QPushButton *button = new QPushButton(this);
+    QIcon icon;
+    icon.addFile(":2.png", QSize(48, 48));
     setCentralWidget(button);
 }
